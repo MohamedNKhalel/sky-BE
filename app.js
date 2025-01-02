fb89cde1f56ff6d9e37afdf937a27b690c4d0894
Mohamed Khalel <mn911415@gmail.com>
2025-01-02 09:16:51 +0200

    modify

--- app.js
+++ app.js
@@ -205,7 +205,7 @@ app.get('/project', authenticateToken, (req, res) => {
 
 // ===================== START SERVER ===================== //
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
